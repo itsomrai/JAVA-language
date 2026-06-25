a2e509364ee2652c1665afb37582f8ddd0b34167
itsomrai <work.omrai@gmail.com>
2026-06-25 19:26:45 +0530

    DAY 1 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day1/swaping.java
@@ -0,0 +1,9 @@
+public class swaping {
+    public static void main(String[] args) {
+        int a=2,b=3;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a="+a+ "b="+b);
+    }
+}

95a4ff5bc79e653503c15a0dfcbd01e05c70703d
itsomrai <work.omrai@gmail.com>
2026-06-27 21:58:27 +0530

    DAY 3 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day3/loopSeriesMul.java
@@ -0,0 +1,9 @@
+public class loopSeriesMul {
+    public static void main(String[] args) {
+        int prod=1;
+        for(int i=1;i<=10;i++){
+            prod=5*i;
+            System.out.println("5 X "+i+"="+prod);
+        }
+    }
+}

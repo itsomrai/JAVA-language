afa2871112dee8eab38073e36c6adc07e023611a
itsomrai <work.omrai@gmail.com>
2026-06-28 21:28:33 +0530

    DAY 4 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day4/reverseDigit.java
@@ -0,0 +1,13 @@
+public class reverseDigit {
+    public static void main(String[] args) {
+        int num=123;
+        int rev=0;
+        int rem;
+        while(num!=0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num/=10;
+        }
+        System.out.println(rev);
+    }
+}

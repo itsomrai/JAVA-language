e29d252656fcb1b1c29c6f665bb6aff4309f2b31
itsomrai <work.omrai@gmail.com>
2026-06-28 20:01:32 +0530

    DAY 4 OF JAVA PRORAMMING

---
+++ 50days-CODING/day4/SumOdd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SumOdd {
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER N VALUES");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
---
+++ 50days-CODING/day4/factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class factorial {
+    public static void main(String[] args) {
+        int fact=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER WHICH YOU HAVE TO FIND ITS FACTORIAL");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        System.out.println("THE FACTORIAL OF "+n+"="+fact);
+        sc.close();
+    }
+}

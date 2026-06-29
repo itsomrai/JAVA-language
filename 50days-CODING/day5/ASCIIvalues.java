ad0273211741c933c2276c041b0fdbad71707b17
itsomrai <work.omrai@gmail.com>
2026-06-29 11:09:21 +0530

    DAY 5 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day5/ASCIIvalues.java
@@ -0,0 +1,7 @@
+public class ASCIIvalues {
+    public static void main(String[] args) {
+        for(int i=1;i<=255;i++){
+            System.out.println("ASCII VALUES OF NUMBER: "+i+" ARE "+(char)i);
+        }
+    }
+}

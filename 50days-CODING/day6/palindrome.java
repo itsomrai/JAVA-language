b24fc0809854fc3eae858eb6c84ba9de97f59f99
itsomrai <work.omrai@gmail.com>
2026-06-30 21:23:43 +0530

    DAY 6 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day6/palindrome.java
@@ -0,0 +1,19 @@
+public class palindrome {
+    public static void main(String[] args) {
+        int num=121;
+        int rev=0;
+        int rem;
+        int temp=num;
+        while(num!=0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(temp==rev){
+            System.out.println(temp+"IS PALINDROME");
+        }
+        else{
+            System.out.println(temp+"NOT PALINDROME");
+        }
+    }
+}

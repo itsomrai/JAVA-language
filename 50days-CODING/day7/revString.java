87d16469cf0ef8a5472e33744d248a3e7d43bfa5
itsomrai <work.omrai@gmail.com>
2026-07-06 00:17:57 +0530

    DAY 7 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day7/revString.java
@@ -0,0 +1,13 @@
+
+public class revString {
+    public static void main(String[] args) {
+        
+    
+    String s="HELLO";
+    String rev="";
+    for(int i=s.length()-1;i>=0;i--){
+        rev+=s.charAt(i);
+    }
+    System.out.println(rev);
+}
+}

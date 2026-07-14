07122342fbaacddb287501c672c70b74e0daac7c
itsomrai <work.omrai@gmail.com>
2026-07-14 21:40:48 +0530

    DAY 11 OF JAVA PROGRAMMING

---
+++ 50days-CODING/day11/SeriesSpl.java
@@ -0,0 +1,8 @@
+public class SeriesSpl {
+    public static void main(String[] args) {
+        for(int i=1;i<=8;i++){
+            int series=i*(i+1);
+            System.out.println(series);
+        }
+    }
+}

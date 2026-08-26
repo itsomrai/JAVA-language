ddb5d4ac5dbf3e01222202427e1df349e3474518
itsomrai <work.omrai@gmail.com>
2026-08-26 20:57:55 +0530

    LEETCODE PROBELUM

---
+++ 50days-CODING/day13/ransomeNote.java
@@ -0,0 +1,12 @@
+public class ransomeNote {
+    public static void main(String[] args) {
+        String ransome="AA";
+        String magazine="AAB";
+        if(magazine.contains(ransome)){
+            System.out.println("TRUE");
+        }
+        else{
+            System.out.println("FALSE");
+        }
+    }
+}
